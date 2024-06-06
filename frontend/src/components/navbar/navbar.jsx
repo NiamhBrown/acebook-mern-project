46400efd64488a7e9c12ca0d4e991c388d66f252
Harry Donnelly <harrydonnelly725@gmail.com>
2024-06-06 09:58:00 +0100

    Added functionaility to the navbar where the number of requests is rendered

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -1,20 +1,55 @@
 // import React from "react";
 import "./navbar.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link , useNavigate} from "react-router-dom";
 import { SearchBar } from "./searchbar/SearchBar";
 import { SearchResultsList } from "./searchbar/SearchResultsList";
 import logo from "../../assets/Mountain-logo.png";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const Navbar =  (token) => {
     const [results, setResults] = useState([]);
     const navigate = useNavigate();
+    const [requests, setRequests] = useState(0)
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const userID = localStorage.getItem("userId")
 
     const handleLogout = () =>{
             localStorage.removeItem("token");
             localStorage.removeItem("userId");
             navigate("/login")
         };
+    useEffect(()=>{
+            const fetchData = async () => {
+                try{
+                const usersResponse = await fetch(`${BACKEND_URL}/users`);
+                console.log("useEffect fetch")
+                if (!usersResponse.ok) {
+                    throw new Error('Network response was not ok ' + usersResponse.statusText);
+                }
+                const usersData = await usersResponse.json();
+                const currentUser = usersData.users.find((user) =>user._id === userID);
+                const requestedUsers = usersData.users.filter(user => 
+                    user.friends.includes(userID) &&
+                    !currentUser.friends.includes(user._id) &&
+                    user._id !== userID
+                )
+
+                console.log('Requested users',requestedUsers)
+                console.log("reqeusted users length", requestedUsers.length)                
+                setRequests(requestedUsers.length); 
+            } catch(error) {
+                    setError(error);
+                    setLoading(false);
+                };
+            }
+            if (token && userID){
+                fetchData()
+            }
+        },[token, userID] )
+
+
 
     return (
         <nav className="navbar" id="navbar">
@@ -33,7 +68,7 @@ const Navbar =  (token) => {
                 </div>
             </div>
             <div className= "navbar-right">
-                <Link to = {"/requests"}>Friend Requests</Link>
+                <Link to = {"/requests"}>Friend Requests({requests})</Link>
                 <Link to = {"/friends"}>Friends</Link>
                 <Link to = {"/profile"}>Profile</Link>
                 <button onClick={handleLogout} id="button1">
