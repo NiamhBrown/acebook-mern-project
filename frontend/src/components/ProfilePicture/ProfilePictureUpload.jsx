39c27d1d00a50e73b9da38037dbe3b1e76fbaba9
niamh <niamh321@me.com>
2024-06-06 11:48:14 +0100

    add profile pic form is now a dropdown

--- frontend/src/components/ProfilePicture/ProfilePictureUpload.jsx
+++ frontend/src/components/ProfilePicture/ProfilePictureUpload.jsx
@@ -3,6 +3,7 @@ import { uploadProfilePicture } from "../../services/users";
 
 const ProfilePictureUpload = ({ token }) => {
     const [profilePicture, setProfilePicture] = useState(null)
+    const [isFormVisible, setIsFormVisible] = useState(false);
 
     const handleFileChange = (event) => {
         setProfilePicture(event.target.files[0]);
@@ -12,16 +13,28 @@ const ProfilePictureUpload = ({ token }) => {
         try {
             const data = await uploadProfilePicture(token, profilePicture);
             console.log("Profile picture uploaded:", data);
+            setIsFormVisible(false);
         } catch (err) {
             console("Error uploading profile picture:", err);
         }
     };
 
     return (
+        // <div>
+        //     <input type="file" onChange={handleFileChange} />
+        //     <button onClick={handleUpload}>Add Profile Picture</button>
+        // </div>
         <div>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload}>Add Profile Picture</button>
-        </div>
+        <button onClick={() => setIsFormVisible(!isFormVisible)}>
+            {isFormVisible ? "Cancel" : "Upload Picture"}
+        </button>
+        {isFormVisible && (
+            <div>
+                <input type="file" onChange={handleFileChange} />
+                <button onClick={handleUpload}>Upload</button>
+            </div>
+        )}
+    </div>
     );
 };
 
